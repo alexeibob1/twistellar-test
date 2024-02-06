a1016c9901f82871689a4d5134e001786aab8995
alexeibob <fingerloom2004@gmail.com>
2024-02-06 22:41:28 +0300

    feat: create raw component displaying owner ID instead of Owner Record

--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 
-export default class ServiceCaseQueueFiltered extends LightningElement {}
+import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
+import OWNER_FIELD from '@salesforce/schema/Case.OwnerId';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
+import PRIORITY_FIELD from '@salesforce/schema/Case.Priority';
+import ORIGIN_FIELD from '@salesforce/schema/Case.Origin';
+import CREATEDDATE_FIELD from '@salesforce/schema/Case.ContactId';
+
+import getUserCases  from '@salesforce/apex/ServiceCaseQueueService.getUserCases';
+
+const COLUMNS = [
+    { label: 'Case Number', fieldName: NUMBER_FIELD.fieldApiName, type: 'text' },
+    { label: 'Assignee', fieldName: OWNER_FIELD.fieldApiName, type: 'text' },
+    { label: 'Case Status', fieldName: STATUS_FIELD.fieldApiName, type: 'text' },
+    { label: 'Priority', fieldName: PRIORITY_FIELD.fieldApiName, type: 'text' },
+    { label: 'Origin', fieldName: ORIGIN_FIELD.fieldApiName, type: 'text' },
+];
+
+export default class ServiceCaseQueueFiltered extends LightningElement {
+    columns = COLUMNS;
+    @wire(getUserCases, {})
+    cases;
+}
