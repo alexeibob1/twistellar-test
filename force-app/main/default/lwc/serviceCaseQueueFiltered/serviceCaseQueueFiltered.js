5252cc830522096c1152b04d5cbdd4f9a32ad511
alexeibob <fingerloom2004@gmail.com>
2024-02-10 10:07:16 +0300

    feat: add Combobox for displaying Case status

--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -2,6 +2,9 @@ import { LightningElement, wire, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
+
+import CASE_OBJECT from "@salesforce/schema/Case";
 
 import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
 import OWNER_ID_FIELD from '@salesforce/schema/Case.OwnerId';
@@ -15,19 +18,22 @@ import CREATED_DATE_FIELD from '@salesforce/schema/Case.CreatedDate';
 import getUserCases from '@salesforce/apex/ServiceCaseQueueService.getUserCases';
 
 const COLUMNS = [
-    { label: 'Case Number', type: 'text' },
-    { label: 'Assignee', type: 'text' },
-    { label: 'Case Status', type: 'text' },
-    { label: 'Priority', type: 'text' },
-    { label: 'Origin', type: 'text' },
-    { label: 'Created at', type: 'date' },
+    { label: 'Case Number'},
+    { label: 'Assignee'},
+    { label: 'Case Status'},
+    { label: 'Priority'},
+    { label: 'Origin'},
+    { label: 'Created at'},
 ];
 
 export default class ServiceCaseQueueFiltered extends NavigationMixin(LightningElement) {
     columns = COLUMNS;
 
-    @wire(getUserCases, {})
+    @wire(getUserCases)
     cases;
+
+    @wire(getPicklistValues, { recordTypeId: "012000000000000AAA", fieldApiName: STATUS_FIELD })
+    statuses;
 
     navigateToCaseRecord(event) {
         this[NavigationMixin.Navigate]({
