19c3be9121f9b56fbaa4e2aace1c320bf4f00dc8
alexeibob <fingerloom2004@gmail.com>
2024-02-11 14:02:40 +0300

    fix: remove unused imports in JS

--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -1,26 +1,14 @@
-import { LightningElement, wire, api, track } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+
 import { NavigationMixin } from 'lightning/navigation';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import { getPicklistValues } from "lightning/uiObjectInfoApi";
 import { updateRecord } from 'lightning/uiRecordApi';
-
-import { refreshApex } from '@salesforce/apex';
-
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-import CASE_OBJECT from "@salesforce/schema/Case";
-
 import ID_FIELD from '@salesforce/schema/Case.Id';
-import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
-import OWNER_ID_FIELD from '@salesforce/schema/Case.OwnerId';
-import OWNER_FIRSTNAME_FIELD from '@salesforce/schema/User.FirstName';
-import OWNER_LASTNAME_FIELD from '@salesforce/schema/User.LastName';
 import STATUS_FIELD from '@salesforce/schema/Case.Status';
-import PRIORITY_FIELD from '@salesforce/schema/Case.Priority';
-import ORIGIN_FIELD from '@salesforce/schema/Case.Origin';
-import CREATED_DATE_FIELD from '@salesforce/schema/Case.CreatedDate';
 
+import { refreshApex } from '@salesforce/apex';
 import getUserCases from '@salesforce/apex/ServiceCaseQueueService.getUserCases';
 
 const COLUMNS = [
